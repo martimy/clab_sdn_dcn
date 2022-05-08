bda9c075c2b43591d3e6d813da7e817f7ed324e1
Maen Artimy <artimym@gmail.com>
2022-05-08 17:27:29 +0000

    add fail-mode setting

--- setup-dc.sh
+++ setup-dc.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo Create bridges
+echo Create switches
 ovs-vsctl --may-exist add-br spine1
 ovs-vsctl --may-exist add-br spine2
 ovs-vsctl --may-exist add-br leaf1
 ovs-vsctl --may-exist add-br leaf2
 ovs-vsctl --may-exist add-br leaf3
 
-echo Set bridge MAC address, or comment for random addresses
-ovs-vsctl set bridge spine1 other-config:hwaddr=00:00:00:00:00:01
-ovs-vsctl set bridge spine2 other-config:hwaddr=00:00:00:00:00:02
-ovs-vsctl set bridge leaf1 other-config:hwaddr=00:00:00:00:00:03
-ovs-vsctl set bridge leaf2 other-config:hwaddr=00:00:00:00:00:04
-ovs-vsctl set bridge leaf3 other-config:hwaddr=00:00:00:00:00:05
+echo Set MAC address, or comment for random addresses
+ovs-vsctl set bridge spine1 other-config:hwaddr=00:00:00:00:01:01
+ovs-vsctl set bridge spine2 other-config:hwaddr=00:00:00:00:01:02
+ovs-vsctl set bridge leaf1 other-config:hwaddr=00:00:00:00:01:03
+ovs-vsctl set bridge leaf2 other-config:hwaddr=00:00:00:00:01:04
+ovs-vsctl set bridge leaf3 other-config:hwaddr=00:00:00:00:01:05
 
-echo Connect switches to each other
+echo Connect switches
 ovs-vsctl add-port spine1 sl_11 -- set interface sl_11 type=patch options:peer=ls_11
 ovs-vsctl add-port spine1 sl_12 -- set interface sl_12 type=patch options:peer=ls_21
 ovs-vsctl add-port spine1 sl_13 -- set interface sl_13 type=patch options:peer=ls_31
@@ -29,13 +29,20 @@ ovs-vsctl add-port leaf2 ls_22 -- set interface ls_22 type=patch options:peer=sl
 ovs-vsctl add-port leaf3 ls_31 -- set interface ls_31 type=patch options:peer=sl_13
 ovs-vsctl add-port leaf3 ls_32 -- set interface ls_32 type=patch options:peer=sl_23
 
-echo Setup the controller and protcols to use
+echo Set switch options
+ovs-vsctl set bridge spine1 fail_mode=secure
+ovs-vsctl set bridge spine2 fail_mode=secure
+ovs-vsctl set bridge leaf1 fail_mode=secure
+ovs-vsctl set bridge leaf2 fail_mode=secure
+ovs-vsctl set bridge leaf3 fail_mode=secure
+
 ovs-vsctl set bridge spine1 protocols=OpenFlow13
 ovs-vsctl set bridge spine2 protocols=OpenFlow13
 ovs-vsctl set bridge leaf1 protocols=OpenFlow13
 ovs-vsctl set bridge leaf2 protocols=OpenFlow13
 ovs-vsctl set bridge leaf3 protocols=OpenFlow13
 
+echo Set controller address
 ovs-vsctl set-controller spine1 tcp:172.10.10.10:6653
 ovs-vsctl set-controller spine2 tcp:172.10.10.10:6653
 ovs-vsctl set-controller leaf1 tcp:172.10.10.10:6653
